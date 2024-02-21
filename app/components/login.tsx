07fc2a84882e10c456194391d07d2aa6e470e394
Lokhnor <vicoporto@gmail.com>
2024-02-21 16:07:07 +1100

    style: eye animation

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef } from 'react';
 import { useState } from 'react';
 import { Stack, useRouter } from 'expo-router';
 import {
@@ -8,6 +9,7 @@ import {
   TouchableOpacity,
   View,
   Image,
+  Animated,
 } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
@@ -24,11 +26,71 @@ export function Login() {
       alert('Incorrect email or password');
     }
   }
+
+  const position = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const moveRight = Animated.timing(position, {
+      toValue: 40,
+      duration: 1000,
+      useNativeDriver: true,
+    });
+
+    const reset = Animated.timing(position, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    });
+
+    const moveLeft = Animated.timing(position, {
+      toValue: -40,
+      duration: 1000,
+      useNativeDriver: true,
+    });
+
+    Animated.loop(
+      Animated.sequence([
+        moveRight,
+        Animated.delay(1000),
+        reset,
+        Animated.delay(1000),
+        moveLeft,
+        Animated.delay(1000),
+        reset,
+        Animated.delay(1000),
+      ]),
+      {
+        iterations: -1,
+      }
+    ).start();
+  }, []);
+
+  const scaleValue = useRef(new Animated.Value(1)).current;
+
+  const buttonScale = () => {
+    Animated.timing(scaleValue, {
+      toValue: 0.95,
+      duration: 100,
+      useNativeDriver: true,
+    }).start(() => {
+      Animated.timing(scaleValue, {
+        toValue: 1,
+        duration: 100,
+        useNativeDriver: true,
+      }).start();
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: 'Login', headerShown: false }} />
-      {/* <Text style={styles.title}>NADA</Text> */}
-      <Image source={require('../../assets/NadaLogo.png')} />
+      <View style={{ justifyContent: 'center' }}>
+        <Image source={require('../../assets/NadaGlobe.png')} />
+        <Animated.Image
+          source={require('../../assets/NadaIris.png')}
+          style={[styles.image, { transform: [{ translateX: position }] }]}
+        />
+      </View>
       <TextInput
         style={styles.textInput}
         onChangeText={onChangeEmail}
@@ -41,9 +103,16 @@ export function Login() {
         placeholder="********"
         value={password}
       />
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
+      <Animated.View style={{ transform: [{ scale: scaleValue }] }}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            handleLogin();
+            buttonScale();
+          }}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </Animated.View>
     </View>
   );
 }
@@ -82,5 +151,11 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     textAlign: 'center',
     width: '100%',
+  },
+  image: {
+    width: 30,
+    height: 30,
+    position: 'absolute',
+    alignSelf: 'center',
   },
 });
