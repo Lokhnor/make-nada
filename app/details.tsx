77d352c872681c41b402213fb98687e5e37f2985
Lokhnor <vicoporto@gmail.com>
2024-02-22 23:10:13 +1100

    feat: timer added

--- app/details.tsx
+++ app/details.tsx
@@ -1,17 +1,51 @@
 import { Stack } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from './components/Button';
+import { useEffect, useState } from 'react';
 
 export default function Details() {
+  const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
+  const [timer, setTimer] = useState(0);
+  const [lastScore, setLastScore] = useState(0);
+
   function startTimer() {
-    alert('Start');
+    setIsTimerRunning(true);
   }
+
+  function stopTimer() {
+    setIsTimerRunning(false);
+    setLastScore(timer);
+    setTimer(0);
+  }
+
+  useEffect(() => {
+    let interval: any = null;
+    if (isTimerRunning) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+      }, 1000);
+    } else if (!isTimerRunning && timer !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isTimerRunning, timer]);
+
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: 'Details', headerShown: false }} />
       <View style={styles.main}>
-        <Text style={styles.title}>Details</Text>
-        <Button title={'Start'} onPress={startTimer}></Button>
+        <Text style={styles.score}>Previous score: {lastScore}</Text>
+        {!isTimerRunning && (
+          <>
+            <Button title={'Start'} onPress={startTimer}></Button>
+          </>
+        )}
+        {isTimerRunning && (
+          <>
+            <Button title={'Stop'} onPress={stopTimer}></Button>
+            <Text style={styles.timer}>{timer}</Text>
+          </>
+        )}
       </View>
     </View>
   );
@@ -30,9 +64,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 64,
+  score: {
+    fontSize: 24,
+    color: 'grey',
+  },
+  timer: {
+    fontSize: 48,
     fontWeight: 'bold',
-    color: 'purple',
+    color: 'white',
   },
 });
